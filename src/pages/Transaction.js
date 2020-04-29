9e6207c4ae5da1631ead25cec252fa3f2138a78d
pk <keattisak.t@ku.th>
2020-04-29 22:29:34 +0700

    choose menu to show table

--- src/pages/Transaction.js
+++ src/pages/Transaction.js
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react'
-import { Form, Row, Col, Button } from 'react-bootstrap'
+import React, { useState, useEffect } from 'react'
+import { Form, Row, Col, DropdownButton, Dropdown } from 'react-bootstrap'
 import axios from 'axios'
 import Blocks from '../components/TransactionList'
 import Table from '../components/TransactionTable'
@@ -7,15 +7,17 @@ import TotalTable from '../components/TotalTable'
 
 function Transaction(props) {
     const [state, setState] = useState({
-        saleList: [] ,
-        buyList: [] ,
+        saleList: [],
+        buyList: [],
         monthSale: -1,
         monthBuy: -1,
         year: "",
-        showBlock: false,
-        firstToggle: false,
-        secondToggle: false,
-        thirdToggle: false,
+    })
+    const [showTable, setShowTable] = useState({
+        block: false,
+        sell: true,
+        buy: false,
+        total: false,
     })
     const [blocks, setBlocks] = useState([])
     const [transactions, setTransaction] = useState({
@@ -26,9 +28,9 @@ function Transaction(props) {
     useEffect(() => {
         const CancelToken = axios.CancelToken
         const source = CancelToken.source()
-        function fetchData(){
-            if(props.role === 'ผู้ดูแลระบบ'){
-                axios.get('https://rubber-backend.herokuapp.com/transactions/blocks', {cancelToken: source.token})
+        function fetchData() {
+            if (props.role === 'ผู้ดูแลระบบ') {
+                axios.get('https://rubber-backend.herokuapp.com/transactions/blocks', { cancelToken: source.token })
                     .then(res => {
                         let data = []
                         res.data.map(obj => {
@@ -39,7 +41,7 @@ function Transaction(props) {
                     .catch(err => {
                         console.log('Error: ', err)
                     })
-                axios.get('https://rubber-backend.herokuapp.com/transactions/', {cancelToken: source.token})
+                axios.get('https://rubber-backend.herokuapp.com/transactions/', { cancelToken: source.token })
                     .then(res => {
                         setTransaction(transactions => ({
                             ...transactions,
@@ -49,30 +51,30 @@ function Transaction(props) {
                     .catch(err => {
                         console.log('Error: ', err)
                     })
-            } else if(props.role === 'เกษตรกร' || props.role === 'พ่อค้าคนกลาง') {
-                axios.get('https://rubber-backend.herokuapp.com/transactions/' + props.firstname + '/' +  props.lastname, {cancelToken: source.token})
-                .then(res => {
-                    setTransaction(res.data)
-                })
-                .catch(err => {
-                    console.log('Error: ', err)
-                })
+            } else if (props.role === 'เกษตรกร' || props.role === 'พ่อค้าคนกลาง') {
+                axios.get('https://rubber-backend.herokuapp.com/transactions/' + props.firstname + '/' + props.lastname, { cancelToken: source.token })
+                    .then(res => {
+                        setTransaction(res.data)
+                    })
+                    .catch(err => {
+                        console.log('Error: ', err)
+                    })
             }
         }
         fetchData()
         return () => {
             source.cancel()
         }
-    },[props])
+    }, [props])
 
     useEffect(() => {
         function filter() {
             let sell = []
             let buy = []
-            if(state.monthSale >= 0) {
+            if (state.monthSale >= 0) {
                 sell = transactions.sale.filter(t => new Date(t.createdAt).getUTCMonth() === Number(state.monthSale))
             }
-            if(state.monthBuy >= 0) {
+            if (state.monthBuy >= 0) {
                 buy = transactions.buy.filter(t => new Date(t.createdAt).getUTCMonth() === Number(state.monthBuy))
             }
             return setState(state => ({
@@ -83,7 +85,7 @@ function Transaction(props) {
         }
         filter()
     }, [state.monthBuy, state.monthSale, transactions])
-    
+
     function handleChange(event) {
         setState({
             ...state,
@@ -91,100 +93,79 @@ function Transaction(props) {
         })
     }
 
-    function toggleBlock() {
-        setState({...state, showBlock: !state.showBlock})
-    }
-
-    function toggleTable(event) {
-        let button
-        if(event.target.name === 'firstToggle'){
-            button = state.firstToggle
-        }else if(event.target.name === 'secondToggle'){
-            button = state.secondToggle
-        }else if(event.target.name === 'thirdToggle'){
-            button = state.thirdToggle
-        }
-        setState({
-            ...state,
-            [event.target.name] : !button
-        })
-    }
-        
     return (
         <div className='container'>
-            <h2>รายงานสรุปการซื้อขาย</h2>
-            <br></br>
-            <Form.Group as={Row} hidden={ state.firstToggle}>
-                <Form.Label column sm={2}> บัญชีการขายยาง</Form.Label>
-                <Col sm={2}>
-                    <Form.Control as='select' name = 'monthSale' onChange={ handleChange}>
-                        <option value = {-1} defaultValue > ทั้งหมด</option>
-                        <option value = {0} > มกราคม</option>
-                        <option value = {1} > กุมภาพันธ์</option>
-                        <option value = {2} > มีนาคม</option>
-                        <option value = {3} > เมษายน</option>
-                        <option value = {4} > พฤษภาคม</option>
-                        <option value = {5} > มิถุนายน</option>
-                        <option value = {6} > กรกฎาคม</option>
-                        <option value = {7} > สิงหาคม</option>
-                        <option value = {8} > กันยายน</option>
-                        <option value = {9} > ตุลาคม</option>
-                        <option value = {10} > พฤศจิกายน</option>
-                        <option value = {11} > ธันวาคม</option>
-                    </Form.Control>
-                </Col>
-            </Form.Group>
             <Row>
+                <h2>รายงานสรุปการซื้อขาย</h2>
                 <Col>
-                    <Button name='firstToggle' onClick={ toggleTable}>ซ่อนตาราง</Button>
+                    <DropdownButton id="dropdown-basic-button" title="รูปแบบการแสดง">
+                        <Dropdown.Item name='sell' onClick={() => setShowTable({ sell: true, block: false, buy: false })}>บัญชีการขายยาง</Dropdown.Item>
+                        {
+                            props.role === 'ผู้ดูแลระบบ' ? <Dropdown.Item onClick={() => setShowTable({ block: true, sell: false, buy: false })}>Blockchain</Dropdown.Item>
+                                : props.role === 'พ่อค้าคนกลาง' ? <Dropdown.Item onClick={() => setShowTable({ buy: true, block: false, sell: false })}>บัญชีการซื้อยาง</Dropdown.Item>
+                                    : <Dropdown.Item onClick={() => setShowTable({ total: true, sell: false })}>บัญชีปริมาณยางคงเหลือ</Dropdown.Item>
+                        }
+
+                    </DropdownButton>
                 </Col>
             </Row>
             <br></br>
-            <Table transactions={ state.monthSale < 0 ?  transactions.sale :  state.saleList} header="รายการขายยาง" hidden={ state.firstToggle}></Table>
-            <br></br>
+            <Form.Group as={Row} hidden={!showTable.sell}>
+                <Form.Label column sm={2}> บัญชีการขายยาง</Form.Label>
+                <Col sm={2}>
+                    <Form.Control as='select' name='monthSale' onChange={handleChange}>
+                        <option value={-1} defaultValue > ทั้งหมด</option>
+                        <option value={0} > มกราคม</option>
+                        <option value={1} > กุมภาพันธ์</option>
+                        <option value={2} > มีนาคม</option>
+                        <option value={3} > เมษายน</option>
+                        <option value={4} > พฤษภาคม</option>
+                        <option value={5} > มิถุนายน</option>
+                        <option value={6} > กรกฎาคม</option>
+                        <option value={7} > สิงหาคม</option>
+                        <option value={8} > กันยายน</option>
+                        <option value={9} > ตุลาคม</option>
+                        <option value={10} > พฤศจิกายน</option>
+                        <option value={11} > ธันวาคม</option>
+                    </Form.Control>
+                </Col>
+            </Form.Group>
+            <Table transactions={state.monthSale < 0 ? transactions.sale : state.saleList} header="รายการขายยาง" hidden={!showTable.sell}></Table>
             {
                 props.role === 'พ่อค้าคนกลาง' ? <>
-                <Form.Group as={Row} hidden={ state.secondToggle}>
-                    <Form.Label column sm={2}> บัญชีการซื้อยาง</Form.Label>
-                    <Col sm={2}>
-                        <Form.Control as='select' name = 'monthBuy' onChange={ handleChange}>
-                            <option value = {-1} defaultValue > ทั้งหมด</option>
-                            <option value = {0} > มกราคม</option>
-                            <option value = {1} > กุมภาพันธ์</option>
-                            <option value = {2} > มีนาคม</option>
-                            <option value = {3} > เมษายน</option>
-                            <option value = {4} > พฤษภาคม</option>
-                            <option value = {5} > มิถุนายน</option>
-                            <option value = {6} > กรกฎาคม</option>
-                            <option value = {7} > สิงหาคม</option>
-                            <option value = {8} > กันยายน</option>
-                            <option value = {9} > ตุลาคม</option>
-                            <option value = {10} > พฤศจิกายน</option>
-                            <option value = {11} > ธันวาคม</option>
-                        </Form.Control>
-                    </Col>
-                </Form.Group>
-                <Row>
-                    <Col>
-                        <Button name='secondToggle' onClick={ toggleTable}>ซ่อนตาราง</Button>
-                    </Col>
-                </Row>
-                <br></br>
-                <Table transactions={ state.monthBuy < 0 ?  transactions.buy :  state.buyList} header="รายการซื้อยาง" hidden={ state.secondToggle}></Table> 
+                    <Form.Group as={Row} hidden={!showTable.buy}>
+                        <Form.Label column sm={2}> บัญชีการซื้อยาง</Form.Label>
+                        <Col sm={2}>
+                            <Form.Control as='select' name='monthBuy' onChange={handleChange}>
+                                <option value={-1} defaultValue > ทั้งหมด</option>
+                                <option value={0} > มกราคม</option>
+                                <option value={1} > กุมภาพันธ์</option>
+                                <option value={2} > มีนาคม</option>
+                                <option value={3} > เมษายน</option>
+                                <option value={4} > พฤษภาคม</option>
+                                <option value={5} > มิถุนายน</option>
+                                <option value={6} > กรกฎาคม</option>
+                                <option value={7} > สิงหาคม</option>
+                                <option value={8} > กันยายน</option>
+                                <option value={9} > ตุลาคม</option>
+                                <option value={10} > พฤศจิกายน</option>
+                                <option value={11} > ธันวาคม</option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
+                    <Table transactions={state.monthBuy < 0 ? transactions.buy : state.buyList} header="รายการซื้อยาง" hidden={!showTable.buy}></Table>
+                </> : null
+            }
+            {
+                props.role === 'ผู้ดูแลระบบ' ? <>
+                    <Blocks transactions={blocks} hidden={!showTable.block}></Blocks>
                 </> : null
             }
             <br></br>
             {
-                    props.role === 'ผู้ดูแลระบบ' ? <>
-                <Button onClick={ toggleBlock}>Show Blocks</Button>
-                <Blocks transactions={ blocks} hidden={! state.showBlock}></Blocks> 
-                </> : null
-            }
-            <br></br>
-            {
-                    props.role !== 'ผู้ดูแลระบบ' && props.role !== 'พ่อค้าคนกลาง' ? <>
-                <TotalTable transactions={ transactions.sale}></TotalTable>
-                </> : null
+                props.role !== 'ผู้ดูแลระบบ' && props.role !== 'พ่อค้าคนกลาง' ? <div hidden={!showTable.total}>
+                    <TotalTable transactions={transactions.sale}></TotalTable>
+                </div> : null
             }
         </div>
     )
